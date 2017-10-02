a3e34027e34edbd0f5310efa232b106249edd229
Ryne Green <32287782+rgreen88@users.noreply.github.com>
2017-10-02 13:18:36 -0400

    Add files via upload
    added additional comments from code dissection to better understand the issue and debug better

--- java/com/example/rynel/navigationdrawer/MainActivity.java
+++ java/com/example/rynel/navigationdrawer/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.rynel.navigationdrawer;
 
-import android.app.ActionBar;
+
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.app.FragmentManager;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -20,19 +16,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import static com.example.rynel.navigationdrawer.PictureActivity.takePicture;
 
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-    //auto generated objects for PictureActivity
-    private ImageView pictureTaken;
-    private Button btnTakePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,44 +30,12 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //PictureActivity object handling for onClick method
-        pictureTaken = (ImageView) findViewById(R.id.ivPicture);
-        btnTakePicture = (Button) findViewById(R.id.btnTakePicture);
 
-        /*attempted action bar customization
-        ActionBar mActionBar = getActionBar();
-        mActionBar.setDisplayShowHomeEnabled(false);
-        mActionBar.setDisplayShowTitleEnabled(false);
-        LayoutInflater mInflater = LayoutInflater.from(this);
-
-        //setting color of action bar
-        mActionBar.setBackgroundDrawable(new ColorDrawable(Color.RED));
-
-
-        View mCustomView = mInflater.inflate(R.layout.custom_actionbar, null);
-        TextView mTitleTextView = (TextView) mCustomView.findViewById(R.id.title_text);
-        mTitleTextView.setText("My Custom Bar!");
-
-        ImageButton imageButton = (ImageButton) mCustomView
-                .findViewById(R.id.imageButton);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-
-
-            //There may be some redundancy
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Refresh Clicked!",
-                        Toast.LENGTH_LONG).show();
-            }
-        });
-
-        mActionBar.setCustomView(mCustomView);
-        mActionBar.setDisplayShowCustomEnabled(true);*/
 
         //pre-made floating e-mail icon with no activity as of yet
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
-        //email floating icon in progress...may do something else like music...maybe icon switching to music theme?
+        //email floating icon doing nothing.  add a function to save/share content maybe?
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -176,7 +133,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     //creating class object to refer to PictureActivity.java
-    /*Intent picture = new Intent();
+   /* Intent picture = new Intent();
     if(picture.onStartActivity(picture)
 
     getPackageManager(MediaStore.ACTION_IMAGE_CAPTURE))!=null)
--- java/com/example/rynel/navigationdrawer/PictureActivity.java
+++ java/com/example/rynel/navigationdrawer/PictureActivity.java
@@ -1,18 +1,31 @@
 package com.example.rynel.navigationdrawer;
 
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+//activity not reaching onClick from first layout to PictureActivity:
+
+//java.lang.IllegalStateException: Could not find method btnTakePicture(View) in a parent or
+// ancestor Context for android:onClick attribute defined on view class
+// android.support.v7.widget.AppCompatButton with id 'btnTakePicture'
 
 public class PictureActivity extends AppCompatActivity {
 
-    //declaring variables holding int 1-100 during drag and holding photo taken
+    //declaring variables for taking photo
     private ImageView pictureTaken;
     static final int takePicture = 1;
 
@@ -28,6 +41,36 @@ public class PictureActivity extends AppCompatActivity {
         pictureTaken = (ImageView) findViewById(R.id.ivPicture);
         btnTakePicture = (Button) findViewById(R.id.btnTakePicture);
 
+
+         //attempted action bar customization
+        ActionBar mActionBar = getActionBar();
+        mActionBar.setDisplayShowHomeEnabled(false);
+        mActionBar.setDisplayShowTitleEnabled(false);
+        LayoutInflater mInflater = LayoutInflater.from(this);
+
+        //setting color of action bar
+        mActionBar.setBackgroundDrawable(new ColorDrawable(Color.RED));
+
+
+        View mCustomView = mInflater.inflate(R.layout.custom_actionbar, null);
+        TextView mTitleTextView = (TextView) mCustomView.findViewById(R.id.title_text);
+        mTitleTextView.setText("My Custom Bar!");
+
+        ImageButton imageButton = (ImageButton) mCustomView
+                .findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+
+
+            //There may be some redundancy
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Refresh Clicked!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        mActionBar.setCustomView(mCustomView);
+        mActionBar.setDisplayShowCustomEnabled(true);
     }
 
 
